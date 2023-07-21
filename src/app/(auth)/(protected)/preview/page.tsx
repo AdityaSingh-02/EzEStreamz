a8d31a253e737f8aacc4dc6331ed01714c8cab26
Aditya Singh <asaditya2002@gmail.com>
2023-07-21 13:37:48 +0530

    feat: copy text

--- src/app/(auth)/(protected)/preview/page.tsx
+++ src/app/(auth)/(protected)/preview/page.tsx
@@ -4,12 +4,15 @@ import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 import { v4 } from "uuid";
 import { useVideo } from "@/Context";
-import {BsCameraVideo, BsCameraVideoOff} from 'react-icons/bs'
+import { BsCameraVideo, BsCameraVideoOff } from "react-icons/bs";
+import { BiCopy } from "react-icons/bi";
+import { MdDone } from "react-icons/md";
 
 const Preview = () => {
   const [video, setVideo] = useState<MediaStream>();
   const [roomId, setRoomId] = useState("");
   const { videoStatus, setVideoStatus } = useVideo();
+  const [copy, setCopyStatus] = useState(false);
 
   let rid = v4().substring(0, 12);
   if (roomId === "") {
@@ -39,6 +42,15 @@ const Preview = () => {
     setVideoStatus(!videoStatus);
   };
 
+  const copyRoomId = () => {
+    navigator.clipboard.writeText(roomId).then(() => {
+      setCopyStatus(true);
+      setTimeout(() => {
+        setCopyStatus(false);
+      }, 1500);
+    })
+  };
+
   return (
     <>
       <div className="flex flex-col md:flex-row justify-start md:justify-around items-center h-screen bg-gradient-to-tr from-blue-500 to-emerald-500">
@@ -48,20 +60,27 @@ const Preview = () => {
         <div className="flex flex-col space-y-5">
           <h1 className="px-4 py-2 rounded-md bg-black mx-2 text-xl">
             Room Id - {roomId}
+            <button className="pl-2 transition-all duration-200" onClick={copyRoomId}>
+              {!copy ? <BiCopy /> : <MdDone color="green" />} 
+            </button>
           </h1>
           <button className="px-4 py-2 rounded-md bg-gray-500 mx-2 text-xl">
             Join
           </button>
           <button
             onClick={toggleCamera}
-            className={`px-4 py-2 flex justify-center rounded-md ${videoStatus ? "bg-red-500" : "bg-gray-500"} mx-2 text-xl`}>
-            {videoStatus? <BsCameraVideoOff /> : <BsCameraVideo />}
+            className={`px-4 py-2 flex justify-center rounded-md ${
+              videoStatus ? "bg-red-500" : "bg-gray-500"
+            } mx-2 text-xl`}>
+            {videoStatus ? <BsCameraVideoOff /> : <BsCameraVideo />}
           </button>
-          <p className="text-black text-sm mx-auto">**Mic is Turned off by default</p>
+          <p className="text-black text-sm mx-auto">
+            **Mic is Turned off by default
+          </p>
         </div>
       </div>
     </>
-  )
+  );
 };
 
 export default Preview;
