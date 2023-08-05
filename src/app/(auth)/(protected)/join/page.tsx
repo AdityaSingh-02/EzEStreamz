36ca2af36dae9d5bfd27b102b1c08ae5234e2be6
Aditya Singh <asaditya2002@gmail.com>
2023-08-05 20:26:11 +0530

    rooms

--- src/app/(auth)/(protected)/join/page.tsx
+++ src/app/(auth)/(protected)/join/page.tsx
@@ -9,6 +9,7 @@ import appwriteService, { AppwriteService } from "@/appwrite-service/config";
 import axios from "axios";
 import { useUserContext } from "@/Context";
 import { usePeer } from "@/Context/usePeer";
+import { useRouter } from "next/navigation";
 
 const Join = () => {
   const [video, setVideo] = useState<MediaStream>();
@@ -20,7 +21,7 @@ const Join = () => {
   const { videoStatus, setVideoStatus } = useVideo();
 
   const { user, addUser } = useUserContext();
-
+  const router = useRouter();
   const { createOffer } = usePeer();
 
   useEffect(() => {
@@ -55,54 +56,56 @@ const Join = () => {
 
   const joinRoom = async () => {
     if (rid.length == 12) {
-      const offer = createOffer();
-      const data: IWebSocketInit = {
-        call: "call-user",
-        email: userInfo.email,
-        name: userInfo.name,
-        rid,
-        offer,
-      };
+      addUser({ user2: userInfo.email, emailUser2: userInfo.email, rid });
+      router.push(`/room/${rid}`);
+      // const offer = createOffer();
+      // const data: IWebSocketInit = {
+      //   call: "call-user",
+      //   email: userInfo.email,
+      //   name: userInfo.name,
+      //   rid,
+      //   offer,
+      // };
 
-      // Post Request For extablishing connection and joining room
-      axios
-        .post("/api/v1/create", data)
-        .then(async (res: any) => {
-          if (res.status == 200) {
-            // Forwarding data with help of hook
-            addUser({ user2: userInfo.email, emailUser2: userInfo.email, rid });
-            createClientConnection(data);
-          }
-        })
-        .catch((err: any) => {
-          console.log(err);
-        });
+      // // Post Request For establishing connection and joining room
+      // axios
+      //   .post("/api/v1/create", data)
+      //   .then(async (res: any) => {
+      //     if (res.status == 200) {
+      //       // Forwarding data with help of hook
+      //       addUser({ user2: userInfo.email, emailUser2: userInfo.email, rid });
+      //       createClientConnection(data);
+      //     }
+      //   })
+      //   .catch((err: any) => {
+      //     console.log(err);
+      //   });
     } else {
       return new Error("Room Id is not valid");
     }
   };
 
-  const createClientConnection = async (data: IWebSocketInit) => {
-    const ws = new WebSocket("ws://localhost:3001");
-    const { call, email, name, rid }: IWebSocketInit = data;
-    ws.onopen = () => {
-      console.log("Connected.");
-      // You can send messages here, as the connection is now open.
-      ws.send(call);
-    };
+  // const createClientConnection = async (data: IWebSocketInit) => {
+  //   const ws = new WebSocket("ws://localhost:3001");
+  //   const { call, email, name, rid }: IWebSocketInit = data;
+  //   ws.onopen = () => {
+  //     console.log("Connected.");
+  //     // You can send messages here, as the connection is now open.
+  //     ws.send(call);
+  //   };
 
-    ws.onmessage = (message) => {
-      console.log(`Received message: ${message.data}`);
-    };
+  //   ws.onmessage = (message) => {
+  //     console.log(`Received message: ${message.data}`);
+  //   };
 
-    ws.onclose = () => {
-      console.log("WebSocket connection closed.");
-    };
+  //   ws.onclose = () => {
+  //     console.log("WebSocket connection closed.");
+  //   };
 
-    ws.onerror = (error) => {
-      console.error("WebSocket error:", error);
-    };
-  };
+  //   ws.onerror = (error) => {
+  //     console.error("WebSocket error:", error);
+  //   };
+  // };
 
   return (
     <>
--- src/app/(auth)/(protected)/room/[rid]/page.tsx
+++ src/app/(auth)/(protected)/room/[rid]/page.tsx
@@ -6,9 +6,10 @@ import type { IRoomUsers } from "@/types/IRoomUsers";
 import type { IWebSocketInit } from "@/types/socketData";
 // import Router from "next/navigation";
 import { usePathname } from "next/navigation";
+import axios from "axios";
 
 const Room = (props: IRoomUsers) => {
-  const { peer, createOffer } = usePeer();
+  const { peer, createOffer, createAnswer } = usePeer();
   const [name, setName] = useState("");
   const [user2, setUser2] = useState("");
 
@@ -18,21 +19,53 @@ const Room = (props: IRoomUsers) => {
   const { user } = useUserContext();
 
   useEffect(() => {
-    handleJoins();
     user.user2 ? setUser2(user.user2) : null;
     if (user2 !== "") {
+      handleJoins();
     }
   }, [user, user2]);
 
   const handleJoins = useCallback(async () => {
     const offer = await createOffer();
-    console.log("offer", rid);
-    // const data : IWebSocketInit = {
-    //   call: "call-user",
-    //   email: user.emailUser1!,
-    //   offer,
-    // }
+    const data: IWebSocketInit = {
+      call: "call-user",
+      email: user.emailUser1!,
+      rid,
+      offer,
+    };
+    axios
+      .post("/api/v1/create", data)
+      .then(async (res: any) => {
+        if (res.status == 200) {
+          createClientConnection(data);
+        }
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
   }, [createOffer]);
+
+  const createClientConnection = async (data: IWebSocketInit) => {
+    const ws = new WebSocket("ws://localhost:3001");
+    const { call, email, name, rid }: IWebSocketInit = data;
+    ws.onopen = () => {
+      ws.send(call);
+    };
+
+    ws.onmessage = async (message) => {
+      console.log(`Received message: ${message.data}`);
+      await createAnswer(message.data);
+    };
+
+    ws.onclose = () => {
+      console.log("WebSocket connection closed.");
+    };
+
+    ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+  };
+
 
   return (
     <>
