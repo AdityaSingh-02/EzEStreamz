9036f48eecc48b708da35c48ef93347d1f75b4f9
AdityaSingh-02 <asaditya2002@gmail.com>
2023-09-22 23:14:13 +0530

    Migrating: Fixing FuckUp

--- src/app/api/v1/temp-create/route.ts
+++ src/app/api/v1/temp-create/route.ts
@@ -1,3 +1,16 @@
-import {NextRequest,NextResponse} from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
+import { Server } from 'socket.io';
 
+export const POST = async (res: Response) => {
+	const io = new Server(res.socket.server);
+	res.socket.server.io = io;
+	io.on('connection', (socket) => {
+		socket.on('send-message', (obj) => {
+			io.emit('recieve-message', obj);
+		});
+	});
+
+	console.log('Connected');
+	res.end();
+};
