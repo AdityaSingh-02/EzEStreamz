a1937f7742f1633ccdb256376444a9a49a596f25
AdityaSingh-02 <asaditya2002@gmail.com>
2024-04-08 15:53:55 +0530

    rm: build files

--- apps/server/dist/uid/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function uuidv4() {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-        var r = (Math.random() * 16) | 0, v = c == "x" ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-    });
-}
-exports.default = uuidv4;
